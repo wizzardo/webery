9da95ba971b8b51090375d744688374bf4c1c95d
MikhailBobrutskov <mbobrutskov@lokata.ru>
2014-04-01 14:57:16 +0400

    small optimizations

--- src/com/wizzardo/httpserver/response/Response.java
+++ src/com/wizzardo/httpserver/response/Response.java
@@ -3,10 +3,8 @@ package com.wizzardo.httpserver.response;
 import com.wizzardo.epoll.readable.ReadableBytes;
 import com.wizzardo.httpserver.ReadableBuilder;
 import com.wizzardo.httpserver.request.Header;
-import simplehttpserver.HttpResponse;
 
-import java.util.LinkedHashMap;
-import java.util.Map;
+import java.util.Arrays;
 
 /**
  * @author: wizzardo
@@ -15,44 +13,83 @@ import java.util.Map;
 public class Response {
     private static final byte[] LINE_SEPARATOR = "\r\n".getBytes();
     private static final byte[] HEADER_SEPARATOR = ": ".getBytes();
-    private Map<byte[], byte[]> headers = new LinkedHashMap<byte[], byte[]>();
 
-    private HttpResponse.Status status = HttpResponse.Status._200;
+    private byte[][] headers = new byte[20][];
+    private int headersCount = 0;
+
+    private Status status = Status._200;
     private byte[] body;
 
     public Response setBody(byte[] body) {
         this.body = body;
+        setHeader(Header.KEY_CONTENT_LENGTH, String.valueOf(body.length));
+        return this;
+    }
+
+    public Response setStatus(Status status) {
+        this.status = status;
+        return this;
+    }
+
+    public Response setHeader(String key, String value) {
+        return setHeader(key.getBytes(), value.getBytes());
+    }
+
+    public Response setHeader(Header key, String value) {
+        return setHeader(key.bytes, value.getBytes());
+    }
+
+    public Response setHeader(Header key, Header value) {
+        return setHeader(key.bytes, value.bytes);
+    }
+
+    public Response setHeader(byte[] key, byte[] value) {
+        for (int i = 0; i < headersCount; i += 2) {
+            if (Arrays.equals(key, headers[i])) {
+                headers[i + 1] = value;
+                return this;
+            }
+        }
+        appendHeader(key, value);
         return this;
     }
 
     public Response appendHeader(String key, String value) {
-        headers.put(key.getBytes(), value.getBytes());
-        return this;
+        return appendHeader(key.getBytes(), value.getBytes());
     }
 
     public Response appendHeader(Header key, String value) {
-        headers.put(key.bytes, value.getBytes());
-        return this;
+        return appendHeader(key.bytes, value.getBytes());
     }
 
     public Response appendHeader(Header key, Header value) {
-        headers.put(key.bytes, value.bytes);
+        return appendHeader(key.bytes, value.bytes);
+    }
+
+    public Response appendHeader(byte[] key, byte[] value) {
+        if (headersCount + 1 >= headers.length)
+            increaseHeadersSize();
+
+        headers[headersCount++] = key;
+        headers[headersCount++] = value;
+
         return this;
+    }
+
+    private void increaseHeadersSize() {
+        byte[][] temp = new byte[headers.length * 3 / 2][];
+        System.arraycopy(headers, 0, temp, 0, headers.length);
+        headers = temp;
     }
 
     public ReadableBytes toReadableByteArray() {
         ReadableBuilder builder = new ReadableBuilder(status.header);
-        for (Map.Entry<byte[], byte[]> header : headers.entrySet()) {
-            builder.append(header.getKey())
+        for (int i = 0; i < headersCount; i += 2) {
+            builder.append(headers[i])
                     .append(HEADER_SEPARATOR)
-                    .append(header.getValue())
+                    .append(headers[i + 1])
                     .append(LINE_SEPARATOR);
         }
-
-        builder.append(Header.KEY_CONTENT_LENGTH.bytes)
-                .append(HEADER_SEPARATOR)
-                .append(String.valueOf(body.length).getBytes())
-                .append(LINE_SEPARATOR);
 
         builder.append(LINE_SEPARATOR);
         builder.append(body);
