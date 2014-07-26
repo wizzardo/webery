e75a96cc8a6de2b3ac2dc9454023bc3f724ca6ef
MikhailBobrutskov <mbobrutskov@lokata.ru>
2014-07-26 17:32:49 +0400

    implement multiple headers with same name

---
+++ src/com/wizzardo/httpserver/HeaderValue.java
@@ -0,0 +1,33 @@
+package com.wizzardo.httpserver;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author: wizzardo
+ * Date: 7/26/14
+ */
+public class HeaderValue {
+    private String value;
+    private List<String> values;
+
+    public HeaderValue(String value) {
+        this.value = value;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public List<String> getValues() {
+        if (values == null) {
+            values = new ArrayList<String>();
+            values.add(value);
+        }
+        return values;
+    }
+
+    public void append(String value) {
+        getValues().add(value);
+    }
+}
