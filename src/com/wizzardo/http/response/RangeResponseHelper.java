0f11883bc91bdf1d388f5fdb8a706991abc21563
wizzardo <wizzardobox@gmail.com>
2014-12-10 21:52:53 +0300

    add etag

--- src/com/wizzardo/http/response/RangeResponseHelper.java
+++ src/com/wizzardo/http/response/RangeResponseHelper.java
@@ -7,6 +7,7 @@ import com.wizzardo.http.request.Header;
 import com.wizzardo.http.request.Request;
 import com.wizzardo.tools.cache.MemoryLimitedCache;
 import com.wizzardo.tools.misc.UncheckedThrow;
+import com.wizzardo.tools.security.MD5;
 
 import java.io.File;
 import java.io.IOException;
@@ -30,7 +31,7 @@ public class RangeResponseHelper {
             FileChannel inChannel = aFile.getChannel();
             ByteBuffer buffer = ByteBuffer.allocateDirect((int) inChannel.size());
             inChannel.read(buffer);
-            return new FileHolder(new ReadableByteBuffer(buffer));
+            return new FileHolder(new ReadableByteBuffer(buffer), MD5.getMD5AsString(s));
         } catch (IOException e) {
             throw UncheckedThrow.rethrow(e);
         }
@@ -38,9 +39,11 @@ public class RangeResponseHelper {
 
     public static class FileHolder implements MemoryLimitedCache.SizeProvider {
         public final ReadableByteBuffer buffer;
+        public final String md5;
 
-        public FileHolder(ReadableByteBuffer buffer) {
+        public FileHolder(ReadableByteBuffer buffer, String md5) {
             this.buffer = buffer;
+            this.md5 = md5;
         }
 
         @Override
@@ -50,10 +53,15 @@ public class RangeResponseHelper {
     }
 
     public static Response makeRangeResponse(Request request, Response response, File file) {
-        response.setHeader(Header.KEY_ACCEPT_RANGES, Header.VALUE_BYTES);
+        response.appendHeader(Header.KEY_ACCEPT_RANGES, Header.VALUE_BYTES);
 
         Range range;
         String rangeHeader = request.header(Header.KEY_RANGE);
+        FileHolder fileHolder = null;
+
+        if (file.length() <= CACHE_FILE_LENGTH_LIMIT)
+            fileHolder = filesCache.get(file.getAbsolutePath());
+
         if (rangeHeader != null) {
             range = new Range(rangeHeader, file.length());
             if (!range.isValid()) {
@@ -62,22 +70,26 @@ public class RangeResponseHelper {
             }
 
             response.setStatus(Status._206);
-            response.setHeader(Header.KEY_CONTENT_RANGE, range.toString());
-            response.setHeader(Header.KEY_CONTENT_LENGTH, range.length());
+            response.appendHeader(Header.KEY_CONTENT_RANGE, range.toString());
+            response.appendHeader(Header.KEY_CONTENT_LENGTH, String.valueOf(range.length()));
         } else {
             Date modifiedSince = request.headerDate(Header.KEY_IF_MODIFIED_SINCE);
-            if (modifiedSince != null && modifiedSince.getTime() >= file.lastModified()) {
-                response.status(Status._304);
-                return response;
-            }
+            if (modifiedSince != null && modifiedSince.getTime() >= file.lastModified())
+                return response.status(Status._304);
+
+            if (fileHolder != null && fileHolder.md5.equals(request.header(Header.KEY_IF_NONE_MATCH)))
+                return response.status(Status._304);
+
             range = new Range(0, file.length() - 1, file.length());
-            response.setHeader(Header.KEY_CONTENT_LENGTH, file.length());
-            response.setHeader(Header.KEY_LAST_MODIFIED, HttpDateFormatterHolder.get().format(new Date(file.lastModified())));
+            response.appendHeader(Header.KEY_CONTENT_LENGTH, String.valueOf(file.length()));
+            if (fileHolder != null)
+                response.appendHeader(Header.KEY_ETAG, fileHolder.md5);
+            response.appendHeader(Header.KEY_LAST_MODIFIED, HttpDateFormatterHolder.get().format(new Date(file.lastModified())));
         }
-        response.setHeader(Header.KEY_CONNECTION, Header.VALUE_CONNECTION_KEEP_ALIVE);
+        response.appendHeader(Header.KEY_CONNECTION, Header.VALUE_CONNECTION_KEEP_ALIVE);
         try {
-            if (file.length() <= CACHE_FILE_LENGTH_LIMIT)
-                response.setBody(filesCache.get(file.getAbsolutePath()).buffer.subBuffer((int) range.from, (int) range.length()));
+            if (fileHolder != null)
+                response.setBody(fileHolder.buffer.subBuffer((int) range.from, (int) range.length()));
             else
                 response.setBody(new ReadableFile(file, range.from, range.length()));
         } catch (IOException e) {
