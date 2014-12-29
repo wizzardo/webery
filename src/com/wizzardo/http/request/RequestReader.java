b339470d358881a74e930a40ba543871d97d5601
wizzardo <wizzardobox@gmail.com>
2014-12-29 21:46:22 +0300

    move AsciiReader to separate package

--- src/com/wizzardo/http/request/RequestReader.java
+++ src/com/wizzardo/http/request/RequestReader.java
@@ -1,7 +1,7 @@
 package com.wizzardo.http.request;
 
 import com.wizzardo.http.MultiValue;
-import com.wizzardo.tools.reflection.StringReflection;
+import com.wizzardo.http.utils.AsciiReader;
 
 import java.io.UnsupportedEncodingException;
 import java.net.URLDecoder;
@@ -397,57 +397,6 @@ public class RequestReader {
 
     public Map<String, MultiValue> getHeaders() {
         return headers;
-    }
-
-    public static class AsciiReader {
-
-        public static String read(byte[] bytes) {
-            return read(bytes, 0, bytes.length);
-        }
-
-        public static String read(byte[] bytes, int offset, int length) {
-            if (length <= 0)
-                return new String();
-
-            int h = 0;
-            int k;
-            char[] data = new char[length];
-            for (int i = 0; i < length; i++) {
-                data[i] = (char) (k = (bytes[offset + i] & 0xff));
-                h = 31 * h + k;
-            }
-
-            return StringReflection.createString(data, h);
-        }
-
-        public static String read(byte[] bytes, int offset, int length, int hash) {
-//            return read(bytes, offset, length);
-            if (length == 0)
-                return new String();
-
-            char[] data = new char[length];
-            for (int i = 0; i < length; i++) {
-                data[i] = (char) (bytes[offset + i] & 0xff);
-            }
-
-            return StringReflection.createString(data, hash);
-        }
-
-        public static String read(byte[] buffer, int bufferLength, byte[] bytes, int offset, int length) {
-            char[] data = new char[bufferLength + length];
-
-            int h = 0;
-            int k;
-            for (int i = 0; i < bufferLength; i++) {
-                data[i] = (char) (k = (buffer[i] & 0xff));
-                h = 31 * h + k;
-            }
-            for (int i = 0; i < length; i++) {
-                data[i + bufferLength] = (char) (k = (bytes[offset + i] & 0xff));
-                h = 31 * h + k;
-            }
-            return StringReflection.createString(data, h);
-        }
     }
 
 }
---
+++ src/com/wizzardo/http/utils/AsciiReader.java
@@ -0,0 +1,57 @@
+package com.wizzardo.http.utils;
+
+import com.wizzardo.tools.reflection.StringReflection;
+
+/**
+* Created by wizzardo on 29.12.14.
+*/
+public class AsciiReader {
+
+    public static String read(byte[] bytes) {
+        return read(bytes, 0, bytes.length);
+    }
+
+    public static String read(byte[] bytes, int offset, int length) {
+        if (length <= 0)
+            return new String();
+
+        int h = 0;
+        int k;
+        char[] data = new char[length];
+        for (int i = 0; i < length; i++) {
+            data[i] = (char) (k = (bytes[offset + i] & 0xff));
+            h = 31 * h + k;
+        }
+
+        return StringReflection.createString(data, h);
+    }
+
+    public static String read(byte[] bytes, int offset, int length, int hash) {
+//            return read(bytes, offset, length);
+        if (length == 0)
+            return new String();
+
+        char[] data = new char[length];
+        for (int i = 0; i < length; i++) {
+            data[i] = (char) (bytes[offset + i] & 0xff);
+        }
+
+        return StringReflection.createString(data, hash);
+    }
+
+    public static String read(byte[] buffer, int bufferLength, byte[] bytes, int offset, int length) {
+        char[] data = new char[bufferLength + length];
+
+        int h = 0;
+        int k;
+        for (int i = 0; i < bufferLength; i++) {
+            data[i] = (char) (k = (buffer[i] & 0xff));
+            h = 31 * h + k;
+        }
+        for (int i = 0; i < length; i++) {
+            data[i + bufferLength] = (char) (k = (bytes[offset + i] & 0xff));
+            h = 31 * h + k;
+        }
+        return StringReflection.createString(data, h);
+    }
+}
