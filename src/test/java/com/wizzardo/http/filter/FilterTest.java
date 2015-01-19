dae346ef0660915f0d44911edf96fa2cc2d47c34
wizzardo <wizzardobox@gmail.com>
2015-01-19 21:20:08 +0300

    improve test

--- src/test/java/com/wizzardo/http/filter/FilterTest.java
+++ src/test/java/com/wizzardo/http/filter/FilterTest.java
@@ -1,6 +1,9 @@
 package com.wizzardo.http.filter;
 
+import com.wizzardo.http.Filter;
 import com.wizzardo.http.ServerTest;
+import com.wizzardo.http.request.Request;
+import com.wizzardo.http.response.Response;
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -21,5 +24,28 @@ public class FilterTest extends ServerTest {
 
         Assert.assertEquals(401, makeRequest("").get().getResponseCode());
         Assert.assertEquals("ok", makeRequest("").setBasicAuthentication(user, password).get().asString());
+    }
+
+    @Test
+    public void testChain() throws IOException {
+        handler = (request, response) -> response.setBody(request.param("all")+" "+request.param("foo")+" "+request.param("bar"));
+        server.getFiltersMapping().addBefore("/*", (request, response) -> {
+            request.param("all", "true");
+            return true;
+        });
+        server.getFiltersMapping().addBefore("/foo/*", (request, response) -> {
+            request.param("foo", "true");
+            return true;
+        });
+        server.getFiltersMapping().addBefore("/foo/bar/*", (request, response) -> {
+            request.param("bar", "true");
+            return true;
+        });
+
+        Assert.assertEquals("true true true", makeRequest("/foo/bar/123").get().asString());
+        Assert.assertEquals("true true true", makeRequest("/foo/bar").get().asString());
+        Assert.assertEquals("true true null", makeRequest("/foo/").get().asString());
+        Assert.assertEquals("true true null", makeRequest("/foo").get().asString());
+        Assert.assertEquals("true null null", makeRequest("/").get().asString());
     }
 }
