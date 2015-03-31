c5cc830396a103a897c42dfac968182a0dd6fac5
wizzardo <wizzardobox@gmail.com>
2015-03-31 20:39:46 +0300

    cache 100 seconds ahead

--- src/main/java/com/wizzardo/http/ServerDate.java
+++ src/main/java/com/wizzardo/http/ServerDate.java
@@ -21,7 +21,12 @@ public class ServerDate {
         DateHolder dateHolder = holder;
         long time = System.currentTimeMillis();
         if (time >= dateHolder.validUntil) {
-            dateHolder = new DateHolder(time);
+            if (time - dateHolder.validUntil < 1000) {
+                dateHolder = dateHolder.next;
+                if (dateHolder == null)
+                    dateHolder = new DateHolder(time);
+            } else
+                dateHolder = new DateHolder(time);
             holder = dateHolder;
         }
         return dateHolder;
@@ -31,11 +36,20 @@ public class ServerDate {
         final String text;
         final byte[] bytes;
         final long validUntil;
+        final DateHolder next;
 
         public DateHolder(long time) {
+            this(time, 0);
+        }
+
+        public DateHolder(long time, int counter) {
             text = "Date: " + HttpDateFormatterHolder.get().format(new Date(time)) + "\r\n";
             bytes = text.getBytes();
             validUntil = (time / 1000 + 1) * 1000;
+            if (counter < 100)
+                next = new DateHolder(time + 1000, counter + 1);
+            else
+                next = null;
         }
 
         public DateHolder() {
