dc4ebceb7c5a3a9f25e9ffd8479ea69072fa6d5d
wizzardo <wizzardobox@gmail.com>
2015-04-24 15:07:23 +0300

    add BytesHolder

---
+++ src/main/java/com/wizzardo/http/template/BytesHolder.java
@@ -0,0 +1,32 @@
+package com.wizzardo.http.template;
+
+import com.wizzardo.epoll.readable.ReadableByteArray;
+import com.wizzardo.epoll.readable.ReadableData;
+
+import java.nio.charset.StandardCharsets;
+import java.util.Map;
+
+/**
+ * @author: moxa
+ * Date: 2/11/13
+ */
+public class BytesHolder implements Renderable {
+    private byte[] bytes;
+
+    public BytesHolder(byte[] bytes) {
+        this.bytes = bytes;
+    }
+
+    public BytesHolder(String s) {
+        this.bytes = s.getBytes(StandardCharsets.UTF_8);
+    }
+
+    public ReadableData get(Map<String, Object> model) {
+        return new ReadableByteArray(bytes);
+    }
+
+    @Override
+    public String toString() {
+        return new String(bytes, StandardCharsets.UTF_8);
+    }
+}
