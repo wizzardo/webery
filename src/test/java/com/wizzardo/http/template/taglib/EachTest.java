fd20422eb54d82f020812ed03e1764c424f5d21e
wizzardo <wizzardobox@gmail.com>
2015-04-26 20:56:33 +0300

    add test_2

--- src/test/java/com/wizzardo/http/template/taglib/EachTest.java
+++ src/test/java/com/wizzardo/http/template/taglib/EachTest.java
@@ -7,6 +7,7 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.util.Arrays;
 import java.util.Collections;
 
 /**
@@ -15,7 +16,7 @@ import java.util.Collections;
 public class EachTest {
 
     @Before
-    public void setup(){
+    public void setup() {
         TagLib.findTags(Collections.singletonList(Each.class));
     }
 
@@ -27,6 +28,27 @@ public class EachTest {
         ViewRenderer.prepare(n.children(), l, "", "");
 
         RenderResult result = l.get(new Model());
+
+        Assert.assertEquals("<div>\n" +
+                "        1\n" +
+                "        <br/>\n" +
+                "        2\n" +
+                "        <br/>\n" +
+                "        3\n" +
+                "        <br/>\n" +
+                "</div>\n", result.toString());
+    }
+
+    @Test
+    public void test_2() {
+        Node n = Node.parse("<div><g:each in=\"$list\">$it<br/></g:each></div>", true);
+
+        RenderableList l = new RenderableList();
+        ViewRenderer.prepare(n.children(), l, "", "");
+
+        Model model = new Model();
+        model.put("list", Arrays.asList(1, 2, 3));
+        RenderResult result = l.get(model);
 
         Assert.assertEquals("<div>\n" +
                 "        1\n" +
