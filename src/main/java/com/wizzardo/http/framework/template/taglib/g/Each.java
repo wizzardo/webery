a55dc111e35d76dd4e852a9bf303cc0588a6018c
wizzardo <wizzardobox@gmail.com>
2015-05-09 11:03:57 +0300

    small refactoring

--- src/main/java/com/wizzardo/http/framework/template/taglib/g/Each.java
+++ src/main/java/com/wizzardo/http/framework/template/taglib/g/Each.java
@@ -1,9 +1,9 @@
 package com.wizzardo.http.framework.template.taglib.g;
 
-import com.wizzardo.http.framework.template.ExpressionHolder;
-import com.wizzardo.http.framework.template.Tag;
 import com.wizzardo.http.framework.template.Body;
+import com.wizzardo.http.framework.template.ExpressionHolder;
 import com.wizzardo.http.framework.template.RenderResult;
+import com.wizzardo.http.framework.template.Tag;
 
 import java.util.Collection;
 import java.util.Map;
@@ -17,12 +17,12 @@ public class Each extends Tag {
     public Each(Map<String, String> attrs, final Body body, String offset) {
         super(attrs, body, offset);
 
-        final ExpressionHolder raw = new ExpressionHolder(attrs.get("in"));
+        ExpressionHolder<Collection> raw = new ExpressionHolder<>(attrs.get("in"));
         Object varName = attrs.get("var");
-        final String var = varName == null ? "it" : String.valueOf(varName);
+        String var = varName == null ? "it" : String.valueOf(varName);
 
         add(model -> {
-            Collection in = (Collection) raw.getRaw(model);
+            Collection in = raw.getRaw(model);
             RenderResult result = new RenderResult();
             for (Object ob : in) {
                 model.put(var, ob);
