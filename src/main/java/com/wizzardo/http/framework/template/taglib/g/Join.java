34c8be1573657945ec3d3247ef14183654182875
wizzardo <wizzardobox@gmail.com>
2015-05-12 15:16:41 +0300

    add Join tag

---
+++ src/main/java/com/wizzardo/http/framework/template/taglib/g/Join.java
@@ -0,0 +1,30 @@
+package com.wizzardo.http.framework.template.taglib.g;
+
+import com.wizzardo.http.framework.template.Body;
+import com.wizzardo.http.framework.template.ExpressionHolder;
+import com.wizzardo.http.framework.template.RenderResult;
+import com.wizzardo.http.framework.template.Tag;
+import com.wizzardo.tools.collections.CollectionTools;
+import com.wizzardo.tools.collections.MapTools;
+
+import java.util.Collection;
+import java.util.Map;
+
+/**
+ * Created by wizzardo on 12.05.15.
+ */
+public class Join extends Tag {
+    public Join(Map<String, String> attrs, Body body, String offset) {
+        super(attrs, body, offset);
+
+        ExpressionHolder<Collection> in = new ExpressionHolder<>(attrs.get("in"));
+        String delimiter = MapTools.getString(attrs, "delimiter", ", ");
+
+        add(model -> {
+            Collection src = in.getRaw(model);
+            RenderResult result = new RenderResult();
+            result.append(CollectionTools.join(src, delimiter));
+            return result;
+        });
+    }
+}
