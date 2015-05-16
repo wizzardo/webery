76c552a72b13f23d8039d7c2c44f1792776ce37d
wizzardo <wizzardobox@gmail.com>
2015-05-16 13:55:40 +0300

    add While tag

---
+++ src/main/java/com/wizzardo/http/framework/template/taglib/g/While.java
@@ -0,0 +1,29 @@
+package com.wizzardo.http.framework.template.taglib.g;
+
+import com.wizzardo.http.framework.template.Body;
+import com.wizzardo.http.framework.template.ExpressionHolder;
+import com.wizzardo.http.framework.template.RenderResult;
+import com.wizzardo.http.framework.template.Tag;
+import com.wizzardo.tools.evaluation.AsBooleanExpression;
+
+import java.util.Collection;
+import java.util.Map;
+
+/**
+ * Created by wizzardo on 16.05.15.
+ */
+public class While extends Tag {
+    public While(Map<String, String> attrs, Body body, String offset) {
+        super(attrs, body, offset);
+
+        ExpressionHolder<Collection> raw = new ExpressionHolder<>(attrs.get("test"));
+
+        add(model -> {
+            RenderResult result = new RenderResult();
+            while (AsBooleanExpression.toBoolean(raw.getRaw(model))) {
+                result.append(body.get(model));
+            }
+            return result;
+        });
+    }
+}
