4ca58bd6d50a2abc57daf1307743067effb49d85
wizzardo <wizzardobox@gmail.com>
2015-05-16 13:55:59 +0300

    add test for While tag

---
+++ src/test/java/com/wizzardo/http/framework/template/taglib/WhileTest.java
@@ -0,0 +1,46 @@
+package com.wizzardo.http.framework.template.taglib;
+
+import com.wizzardo.http.framework.template.*;
+import com.wizzardo.http.framework.template.taglib.g.While;
+import com.wizzardo.tools.xml.Node;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.Collections;
+
+/**
+ * Created by wizzardo on 26.04.15.
+ */
+public class WhileTest {
+
+    @Before
+    public void setup() {
+        TagLib.findTags(Collections.singletonList(While.class));
+    }
+
+    @Test
+    public void test_1() {
+        Node n = Node.parse("<div><g:while test=\"${++i <= 3}\"><p>Current i = ${i}</p></g:while></div>", true);
+
+        RenderableList l = new RenderableList();
+        ViewRenderer.prepare(n.children(), l, "", "");
+
+        Model model = new Model();
+        model.put("i", 0);
+        RenderResult result = l.get(model);
+
+        Assert.assertEquals("" +
+                "<div>\n" +
+                "        <p>\n" +
+                "            Current i = 1\n" +
+                "        </p>\n" +
+                "        <p>\n" +
+                "            Current i = 2\n" +
+                "        </p>\n" +
+                "        <p>\n" +
+                "            Current i = 3\n" +
+                "        </p>\n" +
+                "</div>\n", result.toString());
+    }
+}
