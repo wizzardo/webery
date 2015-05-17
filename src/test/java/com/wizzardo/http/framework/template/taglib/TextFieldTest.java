28b602e0fb1bee34f7551e7972492407b7bd1ac0
wizzardo <wizzardobox@gmail.com>
2015-05-17 13:35:31 +0300

    add tests for TextField tag

---
+++ src/test/java/com/wizzardo/http/framework/template/taglib/TextFieldTest.java
@@ -0,0 +1,75 @@
+package com.wizzardo.http.framework.template.taglib;
+
+import com.wizzardo.http.framework.template.*;
+import com.wizzardo.http.framework.template.taglib.g.TextField;
+import com.wizzardo.tools.xml.Node;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.Collections;
+
+/**
+ * Created by wizzardo on 26.04.15.
+ */
+public class TextFieldTest {
+
+    @Before
+    public void setup() {
+        TagLib.findTags(Collections.singletonList(TextField.class));
+    }
+
+    @Test
+    public void test_1() {
+        Node n = Node.parse("<g:textField name=\"myField\" value=\"${myValue}\"/>", true);
+
+        RenderableList l = new RenderableList();
+        ViewRenderer.prepare(n.children(), l, "", "");
+
+        Model model = new Model();
+        model.put("myValue", 1);
+        RenderResult result = l.get(model);
+
+        Assert.assertEquals("<input type=\"text\" name=\"myField\" id=\"myField\" value=\"1\"/>", result.toString());
+    }
+
+    @Test
+    public void test_2() {
+        Node n = Node.parse("<g:textField name=\"myField\" id=\"text_${myValue}\" value=\"${myValue}\"/>", true);
+
+        RenderableList l = new RenderableList();
+        ViewRenderer.prepare(n.children(), l, "", "");
+
+        Model model = new Model();
+        model.put("myValue", 1);
+        RenderResult result = l.get(model);
+
+        Assert.assertEquals("<input type=\"text\" name=\"myField\" id=\"text_1\" value=\"1\"/>", result.toString());
+    }
+
+    @Test
+    public void test_3() {
+        Node n = Node.parse("<g:textField name=\"myField_${myValue++}\" value=\"${myValue}\"/>", true);
+
+        RenderableList l = new RenderableList();
+        ViewRenderer.prepare(n.children(), l, "", "");
+
+        Model model = new Model();
+        model.put("myValue", 1);
+        RenderResult result = l.get(model);
+
+        Assert.assertEquals("<input type=\"text\" name=\"myField_1\" id=\"myField_1\" value=\"2\"/>", result.toString());
+    }
+
+    @Test
+    public void test_4() {
+        Node n = Node.parse("<g:textField name=\"myField\"/>", true);
+
+        RenderableList l = new RenderableList();
+        ViewRenderer.prepare(n.children(), l, "", "");
+
+        RenderResult result = l.get(new Model());
+
+        Assert.assertEquals("<input type=\"text\" name=\"myField\" id=\"myField\"/>", result.toString());
+    }
+}
