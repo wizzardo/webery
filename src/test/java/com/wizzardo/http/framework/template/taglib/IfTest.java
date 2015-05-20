3772fde3a367ef7ecd37f330675374d6137c62b0
wizzardo <wizzardobox@gmail.com>
2015-05-20 16:11:28 +0300

    improve tests coverage

--- src/test/java/com/wizzardo/http/framework/template/taglib/IfTest.java
+++ src/test/java/com/wizzardo/http/framework/template/taglib/IfTest.java
@@ -106,8 +106,39 @@ public class IfTest {
                 "</div>\n", result.toString());
     }
 
+
     @Test
     public void test_4() {
+        Node n = Node.parse("<g:if test=\"${i == 1}\">one</g:if>" +
+                "<g:elseif test=\"${i == 2}\">two</g:elseif>" +
+                "<g:elseif test=\"${i == 3}\">three</g:elseif>" +
+                "<g:else>else</g:else>", true);
+
+        RenderableList l = new RenderableList();
+        ViewRenderer.prepare(n.children(), l, "", "");
+
+        Model model = new Model();
+        RenderResult result = l.get(model);
+        Assert.assertEquals("    else\n", result.toString());
+
+        model.clear();
+        model.put("i", 1);
+        result = l.get(model);
+        Assert.assertEquals("    one\n", result.toString());
+
+        model.clear();
+        model.put("i", 2);
+        result = l.get(model);
+        Assert.assertEquals("    two\n", result.toString());
+
+        model.clear();
+        model.put("i", 3);
+        result = l.get(model);
+        Assert.assertEquals("    three\n", result.toString());
+    }
+
+    @Test
+    public void test_exceptions() {
         Node n = Node.parse("\"<div><g:if test=\\\"${flag}\\\">text</g:if>error<g:else>foo</g:else></div>\"", true);
         try {
             ViewRenderer.prepare(n.children(), new RenderableList(), "", "");
