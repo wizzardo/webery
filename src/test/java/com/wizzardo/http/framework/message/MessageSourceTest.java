58ac18bb55dcd019fbbefbe0c7dccdbe13559cd2
wizzardo <wizzardobox@gmail.com>
2015-05-23 21:56:22 +0300

    add tests for MessageBundle

--- src/test/java/com/wizzardo/http/framework/message/MessageSourceTest.java
+++ src/test/java/com/wizzardo/http/framework/message/MessageSourceTest.java
@@ -3,6 +3,8 @@ package com.wizzardo.http.framework.message;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.util.Locale;
+
 /**
  * Created by wizzardo on 21.05.15.
  */
@@ -23,5 +25,18 @@ public class MessageSourceTest {
     public void test_properties() {
         MessageSource ms = new PropertiesMessageSource("messages");
         Assert.assertEquals("bar", ms.get("foo"));
+    }
+
+    @Test
+    public void test_messageBundle() {
+        MessageBundle ms = new MessageBundle().load("messages");
+        Assert.assertEquals("bar", ms.get("foo"));
+        Assert.assertEquals("no localization", ms.get("no_localization"));
+
+        Assert.assertEquals("bar ru", ms.get(new Locale("ru", "RU"), "foo"));
+        ms.setDefaultLocale(new Locale("ru", "RU"));
+
+        Assert.assertEquals("bar ru", ms.get("foo"));
+        Assert.assertEquals("no localization", ms.get("no_localization"));
     }
 }
