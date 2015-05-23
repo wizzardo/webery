b6bc24bbbf4df4d2d133533e9c2ed22ed229dd2d
wizzardo <wizzardobox@gmail.com>
2015-05-23 21:10:51 +0300

    TemplateMessageSource

--- src/main/java/com/wizzardo/http/framework/message/PropertiesMessageSource.java
+++ src/main/java/com/wizzardo/http/framework/message/PropertiesMessageSource.java
@@ -9,9 +9,7 @@ import java.util.Properties;
 /**
  * Created by wizzardo on 22.05.15.
  */
-public class PropertiesMessageSource implements MessageSource {
-
-    protected Map<String, Template> templates = new HashMap<>();
+public class PropertiesMessageSource extends TemplateMessageSource {
 
     public PropertiesMessageSource(String name) {
         Properties properties = new Properties();
@@ -20,11 +18,5 @@ public class PropertiesMessageSource implements MessageSource {
         for (Map.Entry<Object, Object> entry : properties.entrySet()) {
             templates.put(String.valueOf(entry.getKey()), new Template(String.valueOf(entry.getValue())));
         }
-    }
-
-    @Override
-    public String get(String key, Object... args) {
-        Template template = templates.get(key);
-        return template == null ? null : template.get(args);
     }
 }
---
+++ src/main/java/com/wizzardo/http/framework/message/TemplateMessageSource.java
@@ -0,0 +1,17 @@
+package com.wizzardo.http.framework.message;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by wizzardo on 23.05.15.
+ */
+public class TemplateMessageSource implements MessageSource {
+    protected Map<String, Template> templates = new HashMap<>();
+
+    @Override
+    public String get(String key, Object... args) {
+        Template template = templates.get(key);
+        return template == null ? null : template.get(args);
+    }
+}
