343f9d4ea598e9cbb6794ce46c819165cee05b04
wizzardo <wizzardobox@gmail.com>
2015-06-24 21:54:51 +0300

    implement lazy writing

--- src/main/java/com/wizzardo/http/ProxyHandler.java
+++ src/main/java/com/wizzardo/http/ProxyHandler.java
@@ -4,6 +4,7 @@ import com.wizzardo.epoll.ByteBufferProvider;
 import com.wizzardo.epoll.Connection;
 import com.wizzardo.epoll.EpollCore;
 import com.wizzardo.epoll.IOThread;
+import com.wizzardo.epoll.readable.ReadableByteArray;
 import com.wizzardo.http.mapping.Path;
 import com.wizzardo.http.request.Header;
 import com.wizzardo.http.request.Request;
@@ -11,16 +12,16 @@ import com.wizzardo.http.response.Response;
 import com.wizzardo.http.response.Status;
 import com.wizzardo.http.utils.AsciiReader;
 import com.wizzardo.tools.misc.ExceptionDrivenStringBuilder;
+import com.wizzardo.tools.misc.Unchecked;
 import com.wizzardo.tools.misc.pool.Holder;
 import com.wizzardo.tools.misc.pool.Pool;
 import com.wizzardo.tools.misc.pool.SoftHolder;
 import com.wizzardo.tools.misc.pool.ThreadLocalPool;
 
 import java.io.IOException;
-import java.util.Arrays;
-import java.util.Map;
-import java.util.Queue;
+import java.util.*;
 import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.atomic.AtomicReference;
 
 /**
  * Created by wizzardo on 28.05.15.
@@ -58,11 +59,7 @@ public class ProxyHandler implements Handler {
             return new IOThread<ProxyConnection>(number, divider) {
                 @Override
                 public void onRead(ProxyConnection connection) {
-                    try {
-                        connection.read(this);
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
+                    connection.read(this);
                 }
             };
         }
@@ -88,66 +85,120 @@ public class ProxyHandler implements Handler {
         Request srcRequest;
         Response srcResponse;
         volatile ResponseReader responseReader;
+        final AtomicReference<Thread> processingBy = new AtomicReference<>();
 
         final byte[] buffer = new byte[16 * 1024];
-        long limit;
+        volatile long limit;
 
         public ProxyConnection(int fd, int ip, int port) {
             super(fd, ip, port);
         }
 
-        protected void read(ByteBufferProvider byteBufferProvider) throws IOException {
-            int r;
-            while ((r = read(buffer, byteBufferProvider)) > 0) {
-                int offset = 0;
-                if (!responseReader.isComplete()) {
-                    int k = responseReader.read(buffer, 0, r);
-                    if (k < 0)
-                        return;
+        protected void read(ByteBufferProvider byteBufferProvider) {
+            Thread current = Thread.currentThread();
+            Thread processing = processingBy.get();
+            if (processing != current && processing != null)
+                processing = null;
 
-                    srcResponse.headersReset();
-                    for (Map.Entry<String, MultiValue> entry : responseReader.headers.entrySet()) {
-                        String name = entry.getKey();
-                        if (entry.getValue().size() > 1)
-                            for (String value : entry.getValue().getValues()) {
-                                srcResponse.appendHeader(name, value);
-                            }
-                        else
-                            srcResponse.appendHeader(name, entry.getValue().getValue());
+            if (!processingBy.compareAndSet(processing, current))
+                return;
+
+            if (!srcRequest.connection().isAlive()) {
+                processingBy.set(null);
+                end();
+                return;
+            }
+            try {
+                int r;
+                byte[] buffer = this.buffer;
+                while ((r = read(buffer, byteBufferProvider)) > 0) {
+                    int offset = 0;
+                    if (!responseReader.isComplete()) {
+                        int k = responseReader.read(buffer, 0, r);
+                        if (k < 0)
+                            continue;
+
+                        srcResponse.headersReset();
+                        for (Map.Entry<String, MultiValue> entry : responseReader.headers.entrySet()) {
+                            String name = entry.getKey();
+                            if (entry.getValue().size() > 1)
+                                for (String value : entry.getValue().getValues()) {
+                                    srcResponse.appendHeader(name, value);
+                                }
+                            else
+                                srcResponse.appendHeader(name, entry.getValue().getValue());
+                        }
+
+                        offset = k;
+                        MultiValue length = responseReader.getHeaders().get(Header.KEY_CONTENT_LENGTH.value);
+                        if (!responseReader.status.equals("200"))
+                            srcResponse.setStatus(Status.valueOf(Integer.parseInt(responseReader.getStatus())));
+
+                        srcResponse.commit(srcRequest.connection());
+                        if (length == null) {
+                            processingBy.set(null);
+                            end();
+                            return;
+                        }
+                        limit = Long.valueOf(length.getValue());
                     }
 
-                    offset = k;
-                    MultiValue length = responseReader.getHeaders().get(Header.KEY_CONTENT_LENGTH.value);
-                    if (!responseReader.status.equals("200"))
-                        srcResponse.setStatus(Status.valueOf(Integer.parseInt(responseReader.getStatus())));
-
-                    srcResponse.commit(srcRequest.connection());
-                    if (length == null) {
-                        end();
-                        break;
-                    }
-                    limit = Long.valueOf(length.getValue());
-                }
-
-                limit -= r - offset;
-                if (limit == 0)
-                    srcRequest.connection().write(buffer, offset, r - offset, byteBufferProvider);
-                else
-                    srcRequest.connection().write(Arrays.copyOfRange(buffer, offset, r), byteBufferProvider);
+                    limit -= r - offset;
+                    if (limit == 0)
+                        proxyWrite(new CustomReadableByteArray(buffer, offset, r - offset), byteBufferProvider);
+                    else
+                        proxyWrite(new CustomReadableByteArray(Arrays.copyOfRange(buffer, offset, r)), byteBufferProvider);
 
 
 //                System.out.println("response: " + new String(buffer, offset, r - offset));
 //                System.out.println("need to read: " + limit + "; r:" + r + " ,offset:" + offset);
-                if (limit == 0) {
-                    end();
+                    if (limit == 0) {
+                        processingBy.set(null);
+                        end();
+                        return;
+                    }
+
                     break;
                 }
+            } catch (Exception e) {
+                try {
+                    close();
+                } catch (IOException ignored) {
+                }
+                try {
+                    srcRequest.connection().close();
+                } catch (IOException ignored) {
+                }
+                e.printStackTrace();
             }
+//            System.out.println("wait "+srcRequest.connection().hasDataToWrite());
+            processingBy.set(null);
+        }
+
+        protected void proxyWrite(CustomReadableByteArray readable, ByteBufferProvider bufferProvider) {
+            srcRequest.connection().write(readable, bufferProvider);
         }
 
         protected void end() {
             srcRequest.connection().onFinishingHandling();
+            limit = -1;
             connections.add(this);
+        }
+
+
+        private class CustomReadableByteArray extends ReadableByteArray {
+            public CustomReadableByteArray(byte[] bytes) {
+                super(bytes);
+            }
+
+            public CustomReadableByteArray(byte[] bytes, int offset, int length) {
+                super(bytes, offset, length);
+            }
+
+            @Override
+            public void onComplete() {
+                Unchecked.run(() -> ProxyConnection.this.read((ByteBufferProvider) Thread.currentThread()));
+            }
         }
     }
 
@@ -162,6 +213,7 @@ public class ProxyHandler implements Handler {
         connection.srcRequest = request;
         connection.srcResponse = response;
         connection.responseReader = new ResponseReader();
+
         try (Holder<ExceptionDrivenStringBuilder> holder = BUILDER_POOL.holder()) {
             ExceptionDrivenStringBuilder requestBuilder = holder.get();
 
@@ -190,7 +242,6 @@ public class ProxyHandler implements Handler {
             connection.write(AsciiReader.write(requestBuilder.toString()), (ByteBufferProvider) Thread.currentThread());
         }
         response.async();
-
 
         return response;
     }
