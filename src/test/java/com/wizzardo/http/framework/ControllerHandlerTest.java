18d7e8b0dd7505225b2b2995f2cdab87cded2645
wizzardo <wizzardobox@gmail.com>
2015-06-30 00:18:33 +0300

    fix test

--- src/test/java/com/wizzardo/http/framework/ControllerHandlerTest.java
+++ src/test/java/com/wizzardo/http/framework/ControllerHandlerTest.java
@@ -1,7 +1,5 @@
 package com.wizzardo.http.framework;
 
-import com.wizzardo.http.ServerTest;
-import com.wizzardo.http.UrlHandler;
 import com.wizzardo.http.framework.template.Renderer;
 import org.junit.Assert;
 import org.junit.Test;
@@ -11,7 +9,7 @@ import java.io.IOException;
 /**
  * Created by wizzardo on 02.05.15.
  */
-public class ControllerHandlerTest extends ServerTest {
+public class ControllerHandlerTest extends WebApplicationTest {
 
     public static class HelloController extends Controller {
 
@@ -22,8 +20,8 @@ public class ControllerHandlerTest extends ServerTest {
 
     @Test
     public void test() throws IOException {
-        handler = new UrlHandler()
-                .append("/hello", new ControllerHandler(HelloController.class, "hello"));
+        server.getUrlMapping()
+                .append("/hello", HelloController.class, "hello");
 
         Assert.assertEquals("hello!", makeRequest("/hello").get().asString());
     }
