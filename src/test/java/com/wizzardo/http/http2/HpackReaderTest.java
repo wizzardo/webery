383fa9a7245fd77e246d9606de48a965575aa3af
wizzardo <wizzardobox@gmail.com>
2015-08-10 20:36:57 +0300

    add first test for int encoding

---
+++ src/test/java/com/wizzardo/http/http2/HpackReaderTest.java
@@ -0,0 +1,21 @@
+package com.wizzardo.http.http2;
+
+import com.wizzardo.http.http2.hpack.HpackReader;
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Created by wizzardo on 10.08.15.
+ */
+public class HpackReaderTest {
+
+    @Test
+    public void test_int_encoding() {
+        byte[] bytes;
+
+        bytes = new byte[1];
+        HpackReader.encode(10, bytes, 5);
+
+        Assert.assertEquals(0b00001010, bytes[0]);
+    }
+}
