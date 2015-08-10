7f363737145edfef66b67e8c929b770dc87daf8b
wizzardo <wizzardobox@gmail.com>
2015-08-10 20:38:55 +0300

    add missed class

---
+++ src/main/java/com/wizzardo/http/http2/hpack/HpackReader.java
@@ -0,0 +1,13 @@
+package com.wizzardo.http.http2.hpack;
+
+/**
+ * Created by wizzardo on 08.08.15.
+ */
+public class HpackReader {
+
+    public static int encode(int i, byte[] bytes, int offsetBits) {
+        int prefix = 8 - (offsetBits % 8);
+
+        return -1;
+    }
+}
