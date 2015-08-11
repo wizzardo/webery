ef3a8de0b651ba5f2c1955fb83a4c2db12c14ae5
wizzardo <wizzardobox@gmail.com>
2015-08-11 22:39:42 +0300

    finish with int encoding

--- src/main/java/com/wizzardo/http/http2/hpack/HpackReader.java
+++ src/main/java/com/wizzardo/http/http2/hpack/HpackReader.java
@@ -8,12 +8,27 @@ public class HpackReader {
     public static int encode(int i, byte[] bytes, int offsetBits) {
         int prefix = 8 - (offsetBits % 8);
         int maxPrefix = (1 << prefix) - 1;
+        int index = offsetBits / 8;
+
         if (i < maxPrefix) {
-            int a = bytes[offsetBits / 8];
+            int a = bytes[index];
             a = a & (((1 << (8 - prefix)) - 1) << prefix);
-            bytes[offsetBits / 8] = (byte) (a | i);
+            bytes[index] = (byte) (a | i);
             return offsetBits + prefix;
         }
-        return -1;
+
+        int a = bytes[index];
+        bytes[index++] = (byte) (a | maxPrefix);
+
+        i -= maxPrefix;
+        while (i >= 128) {
+            a = 128 + (i % 128);
+            bytes[index++] = (byte) a;
+            i /= 128;
+        }
+
+        bytes[index++] = (byte) i;
+
+        return index * 8;
     }
 }
