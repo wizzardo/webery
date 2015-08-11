7d682b9ff02b13fbe097c1f26324e7559ecea4f1
wizzardo <wizzardobox@gmail.com>
2015-08-12 00:15:58 +0300

    improve tests

--- src/test/java/com/wizzardo/http/http2/HpackReaderTest.java
+++ src/test/java/com/wizzardo/http/http2/HpackReaderTest.java
@@ -22,4 +22,26 @@ public class HpackReaderTest {
         Assert.assertEquals(8, HpackReader.encode(10, bytes, 3));
         Assert.assertEquals(0b11101010, bytes[0] & 0xff);
     }
+
+    @Test
+    public void test_int_encoding_2() {
+        byte[] bytes;
+
+        bytes = new byte[3];
+
+        Assert.assertEquals(24, HpackReader.encode(1337, bytes, 3));
+        Assert.assertEquals(0b00011111, bytes[0] & 0xff);
+        Assert.assertEquals(0b10011010, bytes[1] & 0xff);
+        Assert.assertEquals(0b00001010, bytes[2] & 0xff);
+    }
+
+    @Test
+    public void test_int_encoding_3() {
+        byte[] bytes;
+
+        bytes = new byte[1];
+
+        Assert.assertEquals(8, HpackReader.encode(42, bytes, 0));
+        Assert.assertEquals(0b00101010, bytes[0] & 0xff);
+    }
 }
