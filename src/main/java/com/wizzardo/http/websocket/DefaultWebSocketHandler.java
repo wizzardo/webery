02a52800d1def291da38930e4a32b5a8cda249f3
wizzardo <wizzardobox@gmail.com>
2015-12-09 01:23:08 +0300

    add DefaultWebSocketHandler

---
+++ src/main/java/com/wizzardo/http/websocket/DefaultWebSocketHandler.java
@@ -0,0 +1,33 @@
+package com.wizzardo.http.websocket;
+
+import com.wizzardo.http.framework.di.Injectable;
+
+import java.util.Collections;
+import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
+
+/**
+ * Created by wizzardo on 08.12.15.
+ */
+@Injectable
+public class DefaultWebSocketHandler extends WebSocketHandler {
+    protected Set<WebSocketListener> listeners = Collections.newSetFromMap(new ConcurrentHashMap<>());
+
+    @Override
+    public void onConnect(WebSocketListener listener) {
+        listeners.add(listener);
+    }
+
+    @Override
+    public void onDisconnect(WebSocketListener listener) {
+        listeners.remove(listener);
+    }
+
+    public void broadcast(String s) {
+        Message message = new Message(s);
+        for (WebSocketListener listener : listeners) {
+            listener.sendMessage(message);
+        }
+    }
+
+}
