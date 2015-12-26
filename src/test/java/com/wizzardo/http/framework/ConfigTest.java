b90fe615092632f4e9eb780b0c7e3395e935ce34
wizzardo <wizzardobox@gmail.com>
2015-12-26 20:56:16 +0300

    test environments in config

--- src/test/java/com/wizzardo/http/framework/ConfigTest.java
+++ src/test/java/com/wizzardo/http/framework/ConfigTest.java
@@ -1,6 +1,7 @@
 package com.wizzardo.http.framework;
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 import java.io.IOException;
@@ -8,10 +9,49 @@ import java.util.Map;
 
 public class ConfigTest extends WebApplicationTest {
 
+    @Before
+    public void setUp() throws NoSuchMethodException, ClassNotFoundException, NoSuchFieldException {
+        System.out.println("setUp " + name.getMethodName());
+        server = new WebApplication(null, port, context, workers);
+        server.setIoThreadsCount(1);
+    }
+
     @Test
     public void test() throws IOException {
+        server.setEnvironment(Environment.TEST);
+        server.start();
+
         Assert.assertEquals("value", server.getConfig().get("key"));
         Assert.assertEquals("c", ((Map) server.getConfig().get("a")).get("b"));
         Assert.assertEquals(1, ((Map) server.getConfig().get("a")).get("c"));
+
+        Assert.assertTrue(((Map) server.getConfig().get("environments")).isEmpty());
+    }
+
+    @Test
+    public void test_environments_test() throws IOException {
+        server.setEnvironment(Environment.TEST);
+        server.start();
+
+        Assert.assertEquals("test", server.getConfig().get("env"));
+        Assert.assertEquals("test", server.getConfig().get("environment"));
+    }
+
+    @Test
+    public void test_environments_dev() throws IOException {
+        server.setEnvironment(Environment.DEVELOPMENT);
+        server.start();
+
+        Assert.assertEquals("dev", server.getConfig().get("env"));
+        Assert.assertEquals("development", server.getConfig().get("environment"));
+    }
+
+    @Test
+    public void test_environments_prod() throws IOException {
+        server.setEnvironment(Environment.PRODUCTION);
+        server.start();
+
+        Assert.assertEquals("prod", server.getConfig().get("env"));
+        Assert.assertEquals("production", server.getConfig().get("environment"));
     }
 }
--- src/test/resources/Config.groovy
+++ src/test/resources/Config.groovy
@@ -4,3 +4,27 @@ a.b = 'c'
 a {
     c = 1
 }
+
+environments {
+    test {
+        env = 'test'
+    }
+    dev {
+        env = 'dev'
+    }
+    prod {
+        env = 'prod'
+    }
+}
+
+environments {
+    test {
+        environment = 'test'
+    }
+    development {
+        environment = 'development'
+    }
+    production {
+        environment = 'production'
+    }
+}
