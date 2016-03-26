2a1898191a08732dc51072f2192454abe9289a4f
wizzardo <wizzardobox@gmail.com>
2016-03-26 21:35:36 +0300

    small refactoring

--- src/main/java/com/wizzardo/http/framework/di/DependencyScope.java
+++ src/main/java/com/wizzardo/http/framework/di/DependencyScope.java
@@ -4,26 +4,23 @@ package com.wizzardo.http.framework.di;
  * Created by wizzardo on 27.04.15.
  */
 public enum DependencyScope {
-    SINGLETON,
-    PROTOTYPE,
-    SESSION,
-    REQUEST,
-    THREAD_LOCAL;
+    SINGLETON(SingletonDependency::new),
+    PROTOTYPE(PrototypeDependency::new),
+    SESSION(SessionDependency::new),
+    REQUEST(RequestDependency::new),
+    THREAD_LOCAL(ThreadLocalDependency::new);
+
+    private final DependencyFactory factory;
+
+    DependencyScope(DependencyFactory factory) {
+        this.factory = factory;
+    }
 
     public <T> Dependency<T> createDependency(Class<T> clazz) {
-        switch (this) {
-            case SINGLETON:
-                return new SingletonDependency<>(clazz);
-            case PROTOTYPE:
-                return new PrototypeDependency<>(clazz);
-            case SESSION:
-                return new SessionDependency<>(clazz);
-            case THREAD_LOCAL:
-                return new ThreadLocalDependency<>(clazz);
-            case REQUEST:
-                return new RequestDependency<>(clazz);
-            default:
-                throw new IllegalStateException("Unknown scope: " + this);
-        }
+        return factory.create(clazz);
+    }
+
+    private interface DependencyFactory {
+        <T> Dependency<T> create(Class<T> clazz);
     }
 }
