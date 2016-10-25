627208912f488b1968f3fd1b54e8ab90703fd9bc
wizzardo <wizzardobox@gmail.com>
2016-10-25 20:48:46 +0200

    minimal compression level is used by default

--- src/main/java/com/wizzardo/http/filter/GzipFilter.java
+++ src/main/java/com/wizzardo/http/filter/GzipFilter.java
@@ -7,18 +7,30 @@ import com.wizzardo.http.response.Response;
 import com.wizzardo.tools.misc.pool.*;
 
 import java.io.ByteArrayOutputStream;
-import java.util.zip.GZIPOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.util.zip.Deflater;
 
 /**
  * Created by wizzardo on 30.06.15.
  */
 public class GzipFilter implements Filter {
 
-    protected Pool<ByteArrayOutputStream> buffers = new PoolBuilder<ByteArrayOutputStream>()
+    protected final int compressionLevel;
+    protected final Pool<ByteArrayOutputStream> buffers = new PoolBuilder<ByteArrayOutputStream>()
             .supplier(ByteArrayOutputStream::new)
             .resetter(ByteArrayOutputStream::reset)
             .queue(PoolBuilder.createThreadLocalQueueSupplier())
             .build();
+
+    public GzipFilter() {
+        this(Deflater.BEST_SPEED);
+    }
+
+    public GzipFilter(int compressionLevel) {
+        this.compressionLevel = compressionLevel;
+    }
+
 
     @Override
     public boolean filter(Request request, Response response) {
@@ -31,12 +43,19 @@ public class GzipFilter implements Filter {
 
 
         return buffers.provide(out -> {
-            GZIPOutputStream gout = new GZIPOutputStream(out);
+            GZIPOutputStream gout = new GZIPOutputStream(out, compressionLevel);
             gout.write(response.getBody());
             gout.close();
             response.setBody(out.toByteArray());
             response.appendHeader(Header.KV_CONTENT_ENCODING_GZIP);
             return true;
         });
+    }
+
+    static class GZIPOutputStream extends java.util.zip.GZIPOutputStream {
+        public GZIPOutputStream(OutputStream out, int compressionLevel) throws IOException {
+            super(out);
+            def.setLevel(compressionLevel);
+        }
     }
 }
