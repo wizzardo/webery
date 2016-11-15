f8f47422ef87830f6946bda8297aa9075a9eb17e
wizzardo <wizzardobox@gmail.com>
2016-11-15 23:17:30 +0100

    improve tests

--- src/test/java/com/wizzardo/http/framework/template/LocalResourcesToolsTest.java
+++ src/test/java/com/wizzardo/http/framework/template/LocalResourcesToolsTest.java
@@ -5,9 +5,11 @@ import com.wizzardo.tools.io.IOTools;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.concurrent.atomic.AtomicInteger;
 
 /**
  * Created by wizzardo on 14/11/16.
@@ -37,5 +39,41 @@ public class LocalResourcesToolsTest {
         } catch (FileNotFoundException e) {
             Assert.assertEquals("file /tmp/test_resource_2 not found", e.getMessage());
         }
+    }
+
+    @Test
+    public void test_getResourceFile() throws IOException {
+        LocalResourcesTools resources = new LocalResourcesTools();
+
+        FileTools.text("/tmp/test_resource", "foo");
+        AtomicInteger counter = new AtomicInteger();
+        resources.getResourceFile("/tmp/test_resource", file -> {
+            Assert.assertEquals("foo", FileTools.text(file));
+            counter.incrementAndGet();
+        });
+        Assert.assertEquals(1, counter.get());
+
+
+        resources.getResourceFile("/tmp/test_resource_not_exists", file -> {
+            Assert.assertEquals("foo", FileTools.text(file));
+            counter.incrementAndGet();
+        });
+        Assert.assertEquals(1, counter.get());
+    }
+
+    @Test
+    public void test_getResourceFile_2() throws IOException {
+        LocalResourcesTools resources = new LocalResourcesTools();
+
+        Assert.assertEquals("foo", FileTools.text(resources.getResourceFile("test_resource")));
+    }
+
+    @Test
+    public void test_getResourceFile_3() throws IOException {
+        LocalResourcesTools resources = new LocalResourcesTools();
+        resources.addResourcesDir(new File("/tmp"));
+
+        FileTools.text("/tmp/test_resource_foo", "foo");
+        Assert.assertEquals("foo", FileTools.text(resources.getResourceFile("test_resource_foo")));
     }
 }
